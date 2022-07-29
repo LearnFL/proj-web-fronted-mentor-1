415809dc5d92195bf0c7253596a21c3802bf6176
LearnFL <86169204+LearnFL@users.noreply.github.com>
2022-07-29 17:55:49 -0400

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
+
+// Drop-Down Menu
 $(document).ready(function(){
   $(".jq-drop").hover(function(){
     var hovered = $(".jq-drop").is(":hover");
-    if (hovered) {
+    var hov = $(".dropdown-company").is(":hover");
+    if (hovered || hov) {
       $(".dropdown-company").show("fast");
       $('.jq-drop > img').attr("src", "./images/icon-arrow-up.svg");
     }else {
@@ -11,6 +14,7 @@ $(document).ready(function(){
   });
 });
 
+// Arrow Icons
 $(document).ready(function() {
   $('.menu-features').hover(function() {
     var hovered = $('.menu-features').is(":hover");
@@ -20,4 +24,27 @@ $(document).ready(function() {
       $('.menu-features > a > img').attr("src", "./images/icon-arrow-down.svg");
     }
   });
+});
+
+// Show Menu
+$(document).ready(function(){
+  $('.close-icon').hide('fast');
+  if ($('nav').css("visibility") == 'hidden') {
+    $('.menu-bar').click(function(){
+      $('.menu-bar').hide("fast");
+      $('nav').css("visibility", "visible");
+      $('.close-icon').show("fast");
+    });
+  }
+});
+
+// Hide Menu
+$(document).ready(function(){
+  if ($('.menu-bar:hidden') && ($('.close-icon:visible'))) {
+    $('.close-icon').click(function(){
+      $('.close-icon').hide("fast");
+      $('.menu-bar').show("fast");
+      $('nav').css("visibility", "hidden");
+    });
+  }
 });
